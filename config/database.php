f85573f31fdf237f2ef5de2212a3401384d66d14
Kevin <xdarkcaim@gmail.com>
2025-04-28 15:54:28 +0200

    chore(fix): Fix de l'autoloader + rajout de la bibliothéque phpdotenv + rajout check erreur .env + check erreur varriable .env

--- config/database.php
+++ config/database.php
@@ -1,12 +1,38 @@
 <?php
+
+namespace Rtfm2Win\Config;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use PDO;
+use PDOException;
+
+// Vérifier si l'extension PDO est activée
+if (!extension_loaded('pdo')) {
+    die("L'extension PDO n'est pas activée. Veuillez l'activer dans votre fichier php.ini.");
+}
+
 //message d'erreur si l'extension PDO MySQL n'est pas activée
 if (!extension_loaded('pdo_mysql')) {
     die("L'extension PDO MySQL n'est pas activée. Veuillez l'activer dans votre fichier php.ini.");
 }
 
 // Charger les variables d'environnement depuis le fichier .env
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+try {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+} catch (\Exception $e) {
+    die("Erreur : " . $e->getMessage());
+}
+
+// Vérification des variables d'environnement nécessaires
+$requiredEnvVars = ['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'];
+foreach ($requiredEnvVars as $var) {
+    if (empty($_ENV[$var])) {
+        die("Erreur : La variable d'environnement {$var} n'est pas définie dans le fichier .env.");
+    }
+}
 
 //configuration de la connexion à la base de données
 $config = [
