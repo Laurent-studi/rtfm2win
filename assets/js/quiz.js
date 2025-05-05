2758e21c3a3710902c7ef23546ed117850289cb5
c41m07 <xdarkcaim@gmail.com>
2025-05-05 13:08:22 +0200

    Revert "Feature/backend"

--- assets/js/quiz.js
+++
@@ -1,7 +0,0 @@
-const inputPseudo = document.getElementById("pseudoInput")
-const play = document.getElementById("play")
-
-play.addEventListener('click',() => {
-    console.log(inputPseudo.value)
-
-})
