1f3dd1586af80bed361c73c111bf8093a29aa1ab
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-05-07 18:00:15 +0200

    Add check pseudo features
    - Adding a feature to check the pseudo.
    - Adding an UX effect if the pseudo is correct or not.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,20 @@
-const inputPseudo = document.getElementById("pseudoInput")
-const play = document.getElementById ("play")
+const inputPseudo = document.getElementById('pseudo-input');
 
-play.addEventListener('click',() =>{
-    console.log(inputPseudo.value)
+document.querySelector('#join-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const pseudo = document.querySelector('#pseudo-input').value.trim();
     
-})
+    if (validerPseudo(pseudo)) {
+        inputPseudo.setAttribute('class','validPseudo');
+    }
+    else{
+        inputPseudo.setAttribute('class','invalidPseudo')
+    }
+    console.log('test')
+});
+
+function validerPseudo(pseudo) {
+    let regex = /^[a-zA-Z0-9_\-\.]{3,20}$/;
+    return regex.test(pseudo); //retrourne un bool
+}
+
